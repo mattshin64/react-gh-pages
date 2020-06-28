c4aaa381bdb5db7defa79fc5326c00d521d1efff
mattshin64 <mattshin64@gmail.com>
2020-06-28 15:41:45 -0700

    fixed styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import profile from '../profile.png'
+const profilestyle ={
+    borderradius: '50%',
+    width: '250px'
+}
 function Header(){
     return (
     <div className='jumbotron text-center'>
-        <img src={profile} style="border-radius:50%; width: 250px" alt='Me'></img>
+        <img src={profile} style={profilestyle} alt='Me'></img>
         <h1>Matthew Shin</h1>
         <h4>Full Stack Software Engineer</h4>
     </div>

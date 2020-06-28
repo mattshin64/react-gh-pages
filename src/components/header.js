62d4f7666ca565e49dbf9612ca174532dae0aa27
mattshin64 <mattshin64@gmail.com>
2020-06-28 15:31:47 -0700

    added a profile picture

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React from 'react';
 function Header(){
     return (
     <div className='jumbotron text-center'>
+        <img src="../../public/profile.jpg" alt='Me'></img>
         <h1>Matthew Shin</h1>
         <h4>Full Stack Software Engineer</h4>
     </div>

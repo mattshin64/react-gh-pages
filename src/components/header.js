10f389ce5effa780fea871531cc07bac816ba843
mattshin64 <mattshin64@gmail.com>
2020-06-28 15:33:37 -0700

    fixed profile image

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import profile from '../profile.jpg'
 function Header(){
     return (
     <div className='jumbotron text-center'>
-        <img src="../../public/profile.jpg" alt='Me'></img>
+        <img src={profile} alt='Me'></img>
         <h1>Matthew Shin</h1>
         <h4>Full Stack Software Engineer</h4>
     </div>

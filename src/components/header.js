cb976f1c03b0cde4f8ef2fec9b25a9bb963dc3ca
mattshin64 <mattshin64@gmail.com>
2020-06-28 14:55:05 -0700

    added a header section

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Header(){
+    return (
+    <div className='jumbotron text-center'>
+        <h1>Matthew Shin</h1>
+        <h4>Full Stack Software Engineer</h4>
+    </div>
+)
+}
+export default Header;

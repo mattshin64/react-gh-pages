6bb3ab07f1a31b7ea05be6ecd6f61b44ee024c15
mattshin64 <mattshin64@gmail.com>
2020-06-28 16:10:23 -0700

    fixed styling around my experience section

--- src/components/myexperience.js
+++ src/components/myexperience.js
@@ -2,25 +2,20 @@ import React from 'react';
 function MyExperience(){
     return (
         <div>
-            <h2>My Experience</h2>
+            <h2 style={{paddingLeft: 100}}>My Experience</h2>
             <div class="container mt-5 mb-5">
                 <div class="row">
-                    <div class="col-md-6 offset-md-3">
+                    <div class="col-md-6 offset-md-0">
                         <ul class="timeline">
                             <li>
-                                <h5>New Web Design</h5>
-                                <h5 class="float-right">21 March, 2014</h5>
+                                <h5>Hack Reactor</h5>
+                                <h6 class="float-right">October 2019 - January 2020</h6>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
                             </li>
                             <li>
-                                <h5 >21 000 Job Seekers</h5>
-                                <h5 class="float-right">4 March, 2014</h5>
+                                <h5>Volunter Software Engineer at EchoCompassion</h5>
+                                <h6 class="float-right">4 March, 2014</h6>
                                 <p>Curabitur purus sem, malesuada eu luctus eget, suscipit sed turpis. Nam pellentesque felis vitae justo accumsan, sed semper nisi sollicitudin...</p>
-                            </li>
-                            <li>
-                                <h5>Awesome Employers</h5>
-                                <h5 class="float-right">1 April, 2014</h5>
-                                <p>Fusce ullamcorper ligula sit amet quam accumsan aliquet. Sed nulla odio, tincidunt vitae nunc vitae, mollis pharetra velit. Sed nec tempor nibh...</p>
                             </li>
                         </ul>
                     </div>

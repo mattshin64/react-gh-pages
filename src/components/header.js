cbbd801e6879ce367663475b83dd3af3b7eed71b
mattshin64 <mattshin64@gmail.com>
2020-06-28 15:40:12 -0700

    resized profile image and added styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import profile from '../profile.jpg'
+import profile from '../profile.png'
 function Header(){
     return (
     <div className='jumbotron text-center'>
-        <img src={profile} alt='Me'></img>
+        <img src={profile} style="border-radius:50%; width: 250px" alt='Me'></img>
         <h1>Matthew Shin</h1>
         <h4>Full Stack Software Engineer</h4>
     </div>

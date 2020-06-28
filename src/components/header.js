e72046ffdcdb5f86ab89c37edb2fb9bb709a2ae8
mattshin64 <mattshin64@gmail.com>
2020-06-28 15:45:39 -0700

    fixed borderradius

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import profile from '../profile.png'
 const profilestyle ={
-    borderradius: '50%',
+    borderRadius: '50%',
     width: '250px'
 }
 function Header(){

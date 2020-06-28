4846b33c26ef72d8dd8cf1011baf2fc451e8cc87
mattshin64 <mattshin64@gmail.com>
2020-06-28 16:05:12 -0700

    fixed new timeline

--- src/components/myexperience.js
+++ src/components/myexperience.js
@@ -1,30 +1,32 @@
 import React from 'react';
 function MyExperience(){
     return (
-        <div class="container mt-5 mb-5">
-	<div class="row">
-		<div class="col-md-6 offset-md-3">
-			<h4>Latest News</h4>
-			<ul class="timeline">
-				<li>
-					<a target="_blank" href="https://www.totoprayogo.com/#">New Web Design</a>
-					<a href="#" class="float-right">21 March, 2014</a>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
-				</li>
-				<li>
-					<a href="#">21 000 Job Seekers</a>
-					<a href="#" class="float-right">4 March, 2014</a>
-					<p>Curabitur purus sem, malesuada eu luctus eget, suscipit sed turpis. Nam pellentesque felis vitae justo accumsan, sed semper nisi sollicitudin...</p>
-				</li>
-				<li>
-					<a href="#">Awesome Employers</a>
-					<a href="#" class="float-right">1 April, 2014</a>
-					<p>Fusce ullamcorper ligula sit amet quam accumsan aliquet. Sed nulla odio, tincidunt vitae nunc vitae, mollis pharetra velit. Sed nec tempor nibh...</p>
-				</li>
-			</ul>
-		</div>
-	</div>
-</div>
+        <div>
+            <h2>My Experience</h2>
+            <div class="container mt-5 mb-5">
+                <div class="row">
+                    <div class="col-md-6 offset-md-3">
+                        <ul class="timeline">
+                            <li>
+                                <h5>New Web Design</h5>
+                                <h5 class="float-right">21 March, 2014</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
+                            </li>
+                            <li>
+                                <h5 >21 000 Job Seekers</h5>
+                                <h5 class="float-right">4 March, 2014</h5>
+                                <p>Curabitur purus sem, malesuada eu luctus eget, suscipit sed turpis. Nam pellentesque felis vitae justo accumsan, sed semper nisi sollicitudin...</p>
+                            </li>
+                            <li>
+                                <h5>Awesome Employers</h5>
+                                <h5 class="float-right">1 April, 2014</h5>
+                                <p>Fusce ullamcorper ligula sit amet quam accumsan aliquet. Sed nulla odio, tincidunt vitae nunc vitae, mollis pharetra velit. Sed nec tempor nibh...</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default MyExperience;

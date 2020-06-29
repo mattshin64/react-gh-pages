7e247e462609841e59d1b4f54b50dbea44378a62
mattshin64 <mattshin64@gmail.com>
2020-06-28 17:20:03 -0700

    added a table for my skills

--- src/components/myexperience.js
+++ src/components/myexperience.js
@@ -3,19 +3,19 @@ function MyExperience(){
     return (
         <div>
             <h2 style={{paddingLeft: 100}}>My Experience</h2>
-            <div class="container mt-5 mb-5">
+            <div class="container mt-3 mb-5">
                 <div class="row">
                     <div class="col-md-6 offset-md-0">
                         <ul class="timeline">
                             <li>
                                 <h5>Hack Reactor</h5>
-                                <h6 class="float-right">October 2019 - January 2020</h6>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
+                                <h6>October 2019 - January 2020</h6>
+                                <p>12 Week Software Engineering Immersive Program. 65 hour weeks. Graduated with Most Growth Award.</p>
                             </li>
                             <li>
                                 <h5>Volunter Software Engineer at EchoCompassion</h5>
-                                <h6 class="float-right">4 March, 2014</h6>
-                                <p>Curabitur purus sem, malesuada eu luctus eget, suscipit sed turpis. Nam pellentesque felis vitae justo accumsan, sed semper nisi sollicitudin...</p>
+                                <h6>April 2020 - Now</h6>
+                                <p>Handled support tickets that revolved around styling and incorrect back-end API calls.</p>
                             </li>
                         </ul>
                     </div>
--- src/components/myskills.js
+++ src/components/myskills.js
@@ -3,6 +3,57 @@ function MySkills(){
     return (
         <div id='container'>
             <h2 style={{paddingLeft: 100}}>My Skills</h2>
+            <table class='table'>
+                <thead>
+                    <tr>
+                        <th>Front End Technologies</th>
+                        <th>Back End Technologies</th>
+                        <th>Other Technologies</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>JavaScript(ES6)</td>
+                        <td>Node.JS</td>
+                        <td>Azure</td>
+                    </tr>
+                    <tr>
+                        <td>HTML5</td>
+                        <td>Express.JS</td>
+                        <td>AWS(EC2/S3)</td>
+                    </tr>
+                    <tr>
+                        <td>CSS</td>
+                        <td>MySQL</td>
+                        <td>Git</td>
+                    </tr>
+                    <tr>
+                        <td>jQuery</td>
+                        <td>PostgreSQL</td>
+                        <td>Docker</td>
+                    </tr>
+                    <tr>
+                        <td>React.JS</td>
+                        <td>MongoDB</td>
+                        <td>NPM</td>
+                    </tr>
+                    <tr>
+                        <td>Styled Components</td>
+                        <td>Mongoose.JS</td>
+                        <td>Jest</td>
+                    </tr>
+                    <tr>
+                        <td>Babel</td>
+                        <td>Sequelize</td>
+                        <td>Enzyme</td>
+                    </tr>
+                    <tr>
+                        <td>Webpack</td>
+                        <td>Python</td>
+                        <td>NGINX</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
